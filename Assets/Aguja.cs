1af21133bab28289c665b8d807f84fb593f24ba5
Jose Francisco Guerrero <jsfrncscg@gmail.com>
2015-02-06 04:14:33 -0430

    Agregando prueba de movimiento del robot y de impulso de la aguja

---
+++ Assets/Aguja.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aguja : MonoBehaviour {
+
+	public float movspeed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			Debug.Log(-Vector3.up * movspeed);
+
+			rigidbody2D.AddForce(-Vector3.up * movspeed, ForceMode2D.Impulse);
+		}
+	}
+}
---
+++ Assets/Robot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robot : MonoBehaviour {
+
+	public float movspeed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			rigidbody2D.AddForce(-Vector3.right * movspeed);
+		}
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			rigidbody2D.AddForce(Vector3.right * movspeed);
+		}
+
+	}
+
+}
