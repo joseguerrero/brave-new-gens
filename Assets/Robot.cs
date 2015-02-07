15af4f4d4a2c8cc0fa0e068309f393231e77fe60
Jose Francisco Guerrero <jsfrncscg@gmail.com>
2015-02-07 01:43:43 -0430

    - Mejorado el movimiento de la aguja - Trabajando en el movimiento del robot

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -12,17 +12,19 @@ public class Robot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			rigidbody2D.AddForce(-Vector3.right * movspeed);
+			//rigidbody2D.MovePosition(-Vector2.right * movspeed);
+		}
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			rigidbody2D.AddForce(Vector3.right * movspeed);
+			//rigidbody2D.MovePosition(Vector2.right * movspeed);
+		}
 	}
 
 	void FixedUpdate(){
 
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			rigidbody2D.AddForce(-Vector3.right * movspeed);
-		}
-		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			rigidbody2D.AddForce(Vector3.right * movspeed);
-		}
+
 
 	}
 
