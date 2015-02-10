bffd5e046e7d28a5558b99bdae1c2421ad9d14a2
Noel Ramirez T <noeleramirez@yahoo.com>
2015-02-10 13:44:32 -0430

    Agregue escena principal que enlaza con escena de sala de tratamientos.

---
+++ Assets/Scripts/OpenSalaTratamientos.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenSalaTratamientos : MonoBehaviour {
+	private bool loadLock;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && !loadLock) {
+				Application.LoadLevel ("mov-aguja");
+				loadLock=false;
+				}
+
+	}
+}
